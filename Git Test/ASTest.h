8627b4e03fd1bb7bbba546c8fa71c6af22d86cdb
asteff <adam.steff@workstar.com.au>
2014-07-24 07:29:52 +1000

    added a test class

---
+++ Git Test/ASTest.h
@@ -0,0 +1,13 @@
+//
+//  ASTest.h
+//  Git Test
+//
+//  Created by Adam Steff on 24/07/2014.
+//  Copyright (c) 2014 Adam Steff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASTest : NSObject
+
+@end
---
+++ Git Test/ASTest.m
@@ -0,0 +1,13 @@
+//
+//  ASTest.m
+//  Git Test
+//
+//  Created by Adam Steff on 24/07/2014.
+//  Copyright (c) 2014 Adam Steff. All rights reserved.
+//
+
+#import "ASTest.h"
+
+@implementation ASTest
+
+@end
